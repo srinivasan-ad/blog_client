1f6991741b125fbc082158da0685200553df81af
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-02-09 18:01:17 +0530

    blog

---
+++ src/app/components/Quote.tsx
@@ -0,0 +1,22 @@
+
+
+function Quote() {
+  return (
+    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-lg  text-3xl font-bold">
+            Try not to become a man of success, but rather try to become a man of value.
+            </div>
+            </div>
+            <div className="max-w-md text-center text-xl font-semibold">
+                Albert Einstien
+                </div>
+                <div className="max-w-md text-center text-sm font-light text-slate-400">
+                Albert Einstien
+                </div>
+                Scientist
+                </div>
+  )
+}
+
+export default Quote
