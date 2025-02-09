c62f7f4044f814af6c3f51d05f339cdd7d842bab
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-02-09 19:32:07 +0530

    blog

---
+++ src/app/components/Signin.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Quote from './Quote'
+import Create from './Create'
+
+function Signin() {
+  return (
+    <div>
+ <div className='grid grid-cols-2'>
+       <Create type = "signin"/>
+        <Quote/>
+    </div>
+    </div>
+   
+  )
+}
+
+export default Signin
