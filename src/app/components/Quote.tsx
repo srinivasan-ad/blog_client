96cd7f4f9d55b04370488b05727002b91e9ccbe3
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-02-10 19:21:49 +0530

    blog

--- src/app/components/Quote.tsx
+++ src/app/components/Quote.tsx
@@ -1,16 +1,16 @@
 function Quote() {
-    return (
-      <div className="bg-slate-200 h-screen flex items-center justify-center p-4">
-        <div className="max-w-xl text-center">
-          <p className="text-3xl font-bold mb-4">
-            "Try not to become a man of success, but rather try to become a man of value."
-          </p>
-          <p className="text-xl font-semibold mb-2">Albert Einstein</p>
-          <p className="text-sm font-light text-slate-400">Scientist</p>
-        </div>
+  return (
+    <div className="bg-slate-200 h-screen flex items-center justify-center p-4">
+      <div className="max-w-xl text-center">
+        <p className="text-3xl font-bold mb-4">
+          "Try not to become a man of success, but rather try to become a man of
+          value."
+        </p>
+        <p className="text-xl font-semibold mb-2">Albert Einstein</p>
+        <p className="text-sm font-light text-slate-400">Scientist</p>
       </div>
-    );
-  }
-  
-  export default Quote;
-  
+    </div>
+  );
+}
+
+export default Quote;
