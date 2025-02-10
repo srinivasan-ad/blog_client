02dffd08857c71f4926bdd372463d50dae62a7a4
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-02-10 19:21:16 +0530

    blog

--- src/app/components/Blog.tsx
+++ src/app/components/Blog.tsx
@@ -1,13 +1,12 @@
-'use client'
-import { useState } from "react"
-import useBlogs from "../hooks/useBlog"
-import Bar from "./Bar"
-import BlogCard from "./BlogCard"
-import Link from "next/link"
-
+"use client";
+import { useState } from "react";
+import useBlogs from "../hooks/useBlog";
+import Bar from "./Bar";
+import BlogCard from "./BlogCard";
+import Link from "next/link";
 
 function Blog() {
-  const [page,setPage]  = useState(1)
+  const [page, setPage] = useState(1);
 
   function handleNext() {
     setPage((prevPage) => prevPage + 1);
@@ -16,45 +15,49 @@ function Blog() {
   function handlePrev() {
     setPage((prevPage) => (prevPage > 1 ? prevPage - 1 : 1));
   }
-    const {loading,blogs} = useBlogs(page)
-    if(loading)
-    {
-        return(
-        <div>
-            ...loading
-        </div>
-        )
-    }
+  const { loading, blogs } = useBlogs(page);
+  if (loading) {
+    return <div>...loading</div>;
+  }
   return (
     <div>
-<Bar/>
+      <Bar />
 
-    <div className="flex justify-center pt-4">
+      <div className="flex justify-center pt-4">
         <div className=" space-y-10">
-        {blogs.map((blog: any) => (
-          <BlogCard
-          key={blog.id}
-          id = {blog.id}
-          authorName={blog.author_name}
-          title={blog.title}
-          content={blog.content}
-          publishedDate={blog.created_at}
-          />
-        ))}
-   
+          {blogs.map((blog: any) => (
+            <BlogCard
+              key={blog.id}
+              id={blog.id}
+              authorName={blog.author_name}
+              title={blog.title}
+              content={blog.content}
+              publishedDate={blog.created_at}
+            />
+          ))}
         </div>
-    </div>
+      </div>
 
-    <div className="flex justify-center items-center gap-x-5 mt-6">
-        <button onClick={handlePrev} className={`p-2 bg-gray-200 rounded-md ${page === 1 ? "hidden" : "block"}`}>
+      <div className="flex justify-center items-center gap-x-5 mt-6">
+        <button
+          onClick={handlePrev}
+          className={`p-2 bg-gray-200 rounded-md ${
+            page === 1 ? "hidden" : "block"
+          }`}
+        >
           Prev
         </button>
-        <button onClick={handleNext} className={`p-2 bg-gray-200 rounded-md ${blogs.length === 4 ? "block" : "hidden"} `}>
+        <button
+          onClick={handleNext}
+          className={`p-2 bg-gray-200 rounded-md ${
+            blogs.length === 4 ? "block" : "hidden"
+          } `}
+        >
           Next
         </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Blog
+export default Blog;
