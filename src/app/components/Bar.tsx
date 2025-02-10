28b52a2c9f5515e92aed58f83d3a83094c39818a
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-02-10 19:21:31 +0530

    blog

--- src/app/components/Bar.tsx
+++ src/app/components/Bar.tsx
@@ -1,17 +1,14 @@
-import { Avatar } from "./BlogCard"
-
+import { Avatar } from "./BlogCard";
 
 function Bar() {
   return (
     <div className="border-b flex justify-between items-center font-extrabold px-10 py-4">
-        <div>
-            Verbser's Blog
-        </div>
-        <div>
-   <Avatar name = "Aditya" size = "big"/>
-        </div>
+      <div>Verbser's Blog</div>
+      <div>
+        <Avatar name="Aditya" size="big" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Bar
+export default Bar;
