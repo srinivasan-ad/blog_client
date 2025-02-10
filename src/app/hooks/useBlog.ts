8f59147445678169dff2b393db7f01bebfc59411
srinivasan-ad <forprogramingpurpose@gmail.com>
2025-02-10 19:22:25 +0530

    blog

--- src/app/hooks/useBlog.ts
+++ src/app/hooks/useBlog.ts
@@ -1,21 +1,24 @@
-'use client'
-import React, { useEffect, useState } from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
 
-function useBlogs(page : number) {
+function useBlogs(page: number) {
   const [loading, setLoading] = useState<boolean>(true);
   const [blogs, setBlogs] = useState<any[]>([]);
 
   useEffect(() => {
-    async function fetchBlogs(page : number){
+    async function fetchBlogs(page: number) {
       try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/user/blog?page=${page}`, {
-          method: "GET",
-          credentials: "include", 
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-  
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_SERVER_URL}/user/blog?page=${page}`,
+          {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
         const data = await response.json();
         setBlogs(data.blogs || []);
       } catch (error) {
@@ -29,12 +32,9 @@ function useBlogs(page : number) {
     fetchBlogs(page);
   }, []);
 
-
   return {
- 
-      loading, 
-      blogs
- 
+    loading,
+    blogs,
   };
 }
 
